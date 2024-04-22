3acbaa2fd3e491f5eca21bde82a826f2ccf4d440
Atul Patil <atuluttampatil.1757@gmail.com>
2024-04-22 22:28:30 +0530

    nit: minro changes

--- client1/src/Components/Layout/Navbar/Navbar.js
+++ client1/src/Components/Layout/Navbar/Navbar.js
@@ -12,9 +12,9 @@ export const Navbar = () => {
 	return (
 		<div className="h-screen">
 			<div className="flex items-center justify-between p-4 bg-gray-400 h-15 ">
-				<div className="font-medium">CollabCodex</div>
+				<div className="text-xl font-bold">CollabCodeX</div>
 				<div className="flex">
-					<div>Exit Class</div>
+					<div className="pr-4">Exit Class</div>
 					<div onClick={copyClassCode}>Copy Code </div>
 				</div>
 			</div>
